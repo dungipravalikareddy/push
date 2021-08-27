f84d0714bcf046530dac0a1915fe38e8a3d2cc25
dungipravalikareddy <89082767+dungipravalikareddy@users.noreply.github.com>
2021-08-27 15:11:37 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,12 @@
+import os
+List1 = [8, 9, 7, 5, 3]
+List1.reverse()
+print("the reversed list is ", List1)
+
+List2 = [8, 9, 7, 5, 3]
+List2.sort()
+print("the reversed list is ", List2)
+
+List3 = []
+List3 = List1.copy()
+print("List3 =", List1)
